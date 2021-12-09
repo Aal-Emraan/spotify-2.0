f37cbce2985192ea0b286c8a03f165c139ff9d22
Aal Emraan <developer.aalemraan@gmail.com>
2021-12-10 00:25:18 +0600

    updated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,31 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/github"
-import { LOGIN_URL } from "../../../lib/spotify"
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify"
+
+async function refreshAccessToken(token){
+  try{
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const {body: refreshedToken } = spotifyApi.refreshAccessToken;
+    console.log('refresh token is', refreshedToken)
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    }
+  }
+  catch(error){
+      console.log(error)
+
+      return {
+        ...token,
+        error: "RefreshAccessTokenError"
+      }
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,4 +37,37 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login'
+  },
+
+  callbacks: {
+    async jwt({token, user, account}){
+      if(user && account){
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        }
+      }
+
+      if(Date.now() < accessTokenExpires) {
+        return token
+      }
+
+      console.log('access token has expired, refreshing...')
+      return await refreshAccessToken(token)
+    },
+
+    async session ({session, token}){
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.username = token.username;
+
+      return session;
+    }
+  }
 })
