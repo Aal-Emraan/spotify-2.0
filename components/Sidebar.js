420ccb0bf22f238f866672af144caa94dfde2b7a
Aal Emraan <developer.aalemraan@gmail.com>
2021-12-09 22:57:06 +0600

    sidebar added

---
+++ components/Sidebar.js
@@ -0,0 +1,38 @@
+import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon } from '@heroicons/react/outline'
+
+function Sidebar() {
+    return (
+        <div className='p-5 text-gray-500 text-sm border-r border-gray-900'>
+            <div className='space-y-4'>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <HomeIcon className='w-5 h-5'/>
+                    <p>Home</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <SearchIcon className='w-5 h-5'/>
+                    <p>Search</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <LibraryIcon className='w-5 h-5'/>
+                    <p>Your Library</p>
+                </button>
+            <hr className='border-t-[0.1px] border-gray-900'/>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <PlusCircleIcon className='w-5 h-5'/>
+                    <p>Create Playlist</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <HeartIcon className='w-5 h-5'/>
+                    <p>Liked songs</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <RssIcon className='w-5 h-5'/>
+                    <p>Your Episodes</p>
+                </button>
+            <hr className='border-t-[0.1px] border-gray-900'/>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-    <h1>This is spotify clone</h1>
+    <div className="bg-black overflow-hidden h-screen">
+    <main>
+     <Sidebar/>
+    </main>
     </div>
   )
 }
