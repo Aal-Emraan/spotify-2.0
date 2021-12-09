b8666cb0ce12c93bafeaf6e730b90547499a0ced
Aal Emraan <developer.aalemraan@gmail.com>
2021-12-10 01:05:19 +0600

    updated sing in

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,31 @@
-function login() {
+import {getProviders , signIn} from 'next-auth/react'
+
+function login({providers}) {
+    console.log(providers)
     return (
-        <div>
-            <h1>this is login page</h1>
+        <div className='flex justify-center items-center bg-black flex-col min-h-screen'>
+            <img className='mb-5 w-52' src="https://links.papareact.com/9xl" alt="" />
+
+            {Object.values(providers).map((provider) => (
+                <div key={provider.id}>
+                    <button className='bg-[#18D860] text-white p-5 rounded-full'
+                    onClick={() => signIn(provider.id, { callbackUrl: "/"})}
+                    >Login with {provider.name}</button>
+                </div>
+            ))}
+
         </div>
     )
 }
 
-export default login
+export default login;
+
+export async function getServerSideProps() {
+    const providers = await getProviders();
+
+    return {
+        props: {
+            providers,
+        }
+    }
+}
